51ee429bc5269ddbcbfd93b92b06c7a5d7a545ee
aniket2043 <97087872+aniket2043@users.noreply.github.com>
2022-01-09 23:53:53 +0530

    Create ASSIGNMENT-1 OUTPUT.py

---
+++ ASSIGNMENT-1 OUTPUT.py
@@ -0,0 +1,25 @@
+                                                      > python -u "c:\Users\91868\Desktop\PYTHON LEARN PEC\Assignments\ASSIGNMENT_1_INPUT.py"
+                                                INTRODUCTION TO COMPUTING ASSIGNMENT-1
+                                                                PROBLEM-1
+please enter number 1:10
+please enter number 2:12
+please enter number 3:14
+12.0
+                                                                PROBLEM-2
+please enter your Gross Income in dollars:
+20000
+please enter the number of dependentns in your family:
+2
+YOU HAVE TO PAY:
+ 800.0
+                                                                PROBLEM-3
+please enter your details in the following manner:::[SID,Name,Gender,Department Name,CGPA]
+[21105073, 'Aniket Gupta', 'M', 'ECE', 9.2]
+                                                                PROBLEM-4
+Marks of five students
+[12, 23, 43, 45, 56]
+                                                                PROBLEM-5(a)
+['Red', 'Green', 'White', 'Pink', 'Yellow']
+                                                                PROBLEM-5(b)
+['Red', 'Green', 'White', 'Purple', 'Yellow']
+PS C:\Users\91868\Desktop\PYTHON LEARN PEC\Assignments>
