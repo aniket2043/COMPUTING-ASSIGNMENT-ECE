7fa90ac8edb8e72896af233c79e470a5f1d8b32d
aniket2043 <97087872+aniket2043@users.noreply.github.com>
2022-01-21 15:45:31 +0530

    Add files via upload

---
+++ assignment_2_output.py
@@ -0,0 +1,49 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+PS C:\Users\91868\Desktop\PYTHON LEARN PEC\Assignments\ASSIGNMENT 2> python -u "c:\Users\91868\Desktop\PYTHON LEARN PEC\Assignments\ASSIGNMENT 2\assignment_2_input.py"
+PROBLEM-1
+a. 35
+b. egaugnal evitisnes esac a si nohtyP
+c. a case sensitive
+d. Python is object oriented language
+e. 10
+f. Pythonisacasesensitivelanguage
+-----------------------------------------------
+PROBLEM-2
+Hey, Aniket Gupta Here!
+My SID is 21105073
+I am from ECE department and my CGPA is 9.5
+-----------------------------------------------
+PROBLEM-3
+a. a and b is 8
+b. a or b is 58
+c. a XOR b is 50
+d(1). Left Shifting a with 2 bits will give 224
+d(2). Left Shifting b with 2 bits will give 40
+e(1). Right Shifting a with 2 bits will give 14
+e(2). Right Shifting b with 4 bits will give 0
+-----------------------------------------------
+PROBLEM-4
+PLEASE ENTER THE FIRST NUMBER:
+10
+PLEASE ENTER THE SECOND NUMBER:
+12
+PLEASE ENTER THE THIRD NUMBER:
+15
+THE GREATEST OF THREE NUMBERS IS 15
+-----------------------------------------------
+PROBLEM-5
+PLEASE ENTER A STRING:
+name
+Yes
+-----------------------------------------------
+PROBLEM-6
+PLEASE ENTER THE LENGTH OF THE FIRST SIDE:
+1
+PLEASE ENTER THE LENGTH OF THE SECOND SIDE:
+2
+PLEASE ENTER THE LENGTH OF THE THIRD SIDE:
+3
+No
+-----------------------------------------------
+PS C:\Users\91868\Desktop\PYTHON LEARN PEC\Assignments\ASSIGNMENT 2>
