6a2df25e42dc5a33b276818f8c9baa55336419d0
aniket2043 <97087872+aniket2043@users.noreply.github.com>
2022-02-08 14:38:55 +0530

    Create ASSIGNMENT-3 OUTPUT.py

---
+++ ASSIGNMENT-3 OUTPUT.py
@@ -0,0 +1,117 @@
+PS C:\Users\91868\Desktop\PYTHON LEARN PEC\Assignments\ASSIGNMENT 3> python -u "c:\Users\91868\Desktop\PYTHON LEARN PEC\Assignments\ASSIGNMENT 3\assignment_3_input.py"
+**************************ASSIGNMENT-3*************************
+#################################################################
+**************************QUESTION-1****************************
+THIS IS A PROGRAM TO FIND THE NUMBER OF OCCURRENCES WORDS/CHRACTERS IN STRING.
+PLEASE ENTER A STRING=
+
+PLEASE ENTER A VALID STRING!!
+PLEASE ENTER A STRING=
+scssc
+{'s': 3, 'c': 2}
+#################################################################
+**************************QUESTION-2****************************
+THIS IS A PROGRAM TO FIND THE NEXT DATE FOR A GIVEN DATE
+
+PLEASE ENTER THE DAY:
+30
+PLEASE ENTER THE MONTH:
+12
+PLEASE ENTER THE YEAR:
+2002
+THE NEXT DAY FROM THIS DAY WOULD BE 31/12/2002
+#################################################################
+**************************QUESTION-3****************************
+THIS IS A PROGRAM TO FIND THE SQUARES OF A LIST OF NUMBERS.
+PLEASE ENTER A NUMBER :
+5
+DO YOU WANT ANOTHER NUMBER?
+REPLY IN 'Y'OR'N':y
+PLEASE ENTER A NUMBER :
+6
+DO YOU WANT ANOTHER NUMBER?
+REPLY IN 'Y'OR'N':y
+PLEASE ENTER A NUMBER :
+7
+DO YOU WANT ANOTHER NUMBER?
+REPLY IN 'Y'OR'N':y
+PLEASE ENTER A NUMBER :
+8
+DO YOU WANT ANOTHER NUMBER?
+REPLY IN 'Y'OR'N':n
+dict_items([(5, 25), (6, 36), (7, 49), (8, 64)])
+#################################################################
+**************************QUESTION-4****************************
+THIS IS A PROGRAM TO KNOW YOUR GRADE AND PERFORMANCE FROM A GIVEN GRADE POINT.
+PLEASE ENTER YOUR GRADE POINTS:
+6
+Your Grade is ‘C+’ and Average Performance.
+#################################################################
+**************************QUESTION-5****************************
+ ABCDEFGHIJK
+  ABCDEFGHI
+   ABCDEFG
+    ABCDE
+     ABC
+      A
+#################################################################
+**************************QUESTION-6****************************
+THIS IS A PROGRAM TO SAVE THE NAME AND SID OF STUDENTS IN A DICTONARY.
+PLEASE ENTER NAME OF STUDENT:
+ANIKET
+PLEASE ENTER SID OF STUDENT:
+21105073
+DO YOU WANT ANOTHER NAME?
+REPLY IN 'Y'OR'N':Y
+PLEASE ENTER NAME OF STUDENT:
+PRASHANT
+PLEASE ENTER SID OF STUDENT:
+21105072
+DO YOU WANT ANOTHER NAME?
+REPLY IN 'Y'OR'N':Y
+PLEASE ENTER NAME OF STUDENT:
+MUKUL
+PLEASE ENTER SID OF STUDENT:
+21105071
+DO YOU WANT ANOTHER NAME?
+REPLY IN 'Y'OR'N':N
+6.a
+
+{21105073: 'ANIKET', 21105072: 'PRASHANT', 21105071: 'MUKUL'}
+6.b
+
+{21105073: 'ANIKET', 21105071: 'MUKUL', 21105072: 'PRASHANT'}
+6.c
+
+[(21105071, 'MUKUL'), (21105072, 'PRASHANT'), (21105073, 'ANIKET')]
+6.d
+
+GIVEN BELOW IS THE LIST OF SID OF STUDENTS
+[21105071, 21105072, 21105073]
+PLEASE ENTER THE SID OF STUDENT YOU WANT TO FIND.21105073
+ANIKET
+#################################################################
+**************************QUESTION-7****************************
+THIS IS A PROGRAM TO PRINT A FIBONACCI SEQUENCE.
+PLEASE ENTER THE VALUE UPTO WHICH YOU WANT TO PRINT THE SEQUENCE:
+10
+THE FIBONACCI SEQUENCE UPTO 10 TERMS IS:
+0
+1
+1
+2
+3
+5
+8
+13
+21
+34
+#################################################################
+**************************QUESTION-8****************************
+8.a:{1, 3, 5, 6, 8}
+8.b:{17, 3, 6, 8, 9, 13}
+8.c:{1, 2, 3, 4, 5, 6, 8, 9, 13, 17}
+8.d:{6, 7, 8, 9, 10}
+8.e:{10, 7}
+#################################################################
+PS C:\Users\91868\Desktop\PYTHON LEARN PEC\Assignments\ASSIGNMENT 3>
