5f3fbfe165fd53869248b3b8a97e4abc02eb6265
aniket2043 <97087872+aniket2043@users.noreply.github.com>
2022-03-02 00:26:02 +0530

    Create assignment_4_output.py

---
+++ assignment_4_output.py
@@ -0,0 +1,86 @@
+**************************ASSIGNMENT-4*************************
+#################################################################   
+**************************QUESTION-1****************************    
+Move disk 1 from source A to destination B
+Move disk 2 from source A to destination C
+Move disk 1 from source B to destination C
+Move disk 3 from source A to destination B
+Move disk 1 from source C to destination A
+Move disk 2 from source C to destination B
+Move disk 1 from source A to destination B
+#################################################################   
+**************************QUESTION-2****************************    
+PASCAL TRIANGLE USING RECURSSION
+PLEASE ENTER HOW MANY ROWS OF PASCAL TRIANGLE YOU WANT:
+6
+       1 
+      1 1
+     1 2 1
+    1 3 3 1
+   1 4 6 4 1
+  1 5 10 10 5 1
+PLEASE ENTER HOW MANY ROWS OF PASCAL TRIANGLE YOU WANT:
+5
+      1
+     1 1
+    1 2 1
+   1 3 3 1
+  1 4 6 4 1
+ 1 5 10 10 5 1
+#################################################################   
+**************************QUESTION-3****************************    
+PLEASE ENTER FIRST INTEGER:
+10
+PLEASE ENTER SECOND INTEGER:
+3
+THE QUOTIENT ON DIVIDING  NUM1 BY NUM2 IS: 3
+THE REMAINDER ON DIVIDING  NUM1 BY NUM2 IS: 1
+3.a
+False
+False
+True
+3.b
+False
+3.c
+[5, 6]
+3.d
+{5, 6}
+3.e
+frozenset({5, 6})
+3.f
+THE MAXIMUM VALUE IN THE SET IS: 6
+THE HASH VALUE IN THE SET IS: 6
+#################################################################   
+**************************QUESTION-4****************************    
+Student Name: Aniket
+Student Name: 21105073
+#################################################################   
+**************************QUESTION-5****************************    
+Employee : 1
+Employee Name : Mehak
+Employee Salary : 40000
+Employee : 2
+Employee Name : Ashok
+Employee Salary : 50000
+Employee : 3
+Employee Name : Viren
+Employee Salary : 60000
+5.a
+Employee : 1
+Employee Name : Mehak
+Employee Salary : 70000
+5.b
+#################################################################   
+**************************QUESTION-6****************************    
+PLEASE ENTER YOUR WORD GEORGE:
+RUN
+PLEASE ENTER YOUR WORD BARBIE:
+URN
+Does Barbie's word make sense?reply in y or n:
+Y
+{'R': 1, 'U': 1, 'N': 1}
+{'U': 1, 'R': 1, 'N': 1}
+CONGRATULATIONS,YOUR FRIENDSHIP IS TRUE!!!!
+#################################################################   
+Destrucor called,Student deleted
+PS C:\Users\91868\Desktop\PYTHON LEARN PEC\Assignments\ASSIGNMENT 4>
